16a9e6efcc0e3f42b5fec9018651beb1e31ae881
leeski <540470452@qq.com>
2025-07-02 16:14:53 +0800

    init

---
+++ backend/server/src/agent/graph.py
@@ -0,0 +1,69 @@
+"""LangGraph single-node graph template.
+
+Returns a predefined response. Replace logic and configuration as needed.
+"""
+
+from __future__ import annotations
+
+from email import message
+from typing import Any, Dict, TypedDict
+
+from langchain_core.runnables import RunnableConfig
+from langgraph.graph import StateGraph
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import AIMessage
+import os
+from dotenv import load_dotenv
+from typing_extensions import Annotated
+import operator
+
+load_dotenv()
+
+api_key = os.getenv("OPENROUTER_API_KEY")
+base_url = os.getenv("OPENROUTER_BASE_URL")
+model = os.getenv("MODEL_NAME")
+
+llm = ChatOpenAI(model=model,temperature=0.01,timeout=None,max_retries=2,api_key=api_key,base_url=base_url)
+
+class Configuration(TypedDict):
+    """Configurable parameters for the agent.
+
+    Set these when creating assistants OR when invoking the graph.
+    See: https://langchain-ai.github.io/langgraph/cloud/how-tos/configuration_cloud/
+    """
+
+    my_configurable_param: str
+
+
+class State(TypedDict):
+    """Input state for the agent.
+
+    Defines the initial structure of incoming data.
+    See: https://langchain-ai.github.io/langgraph/concepts/low_level/#state
+    """
+    query:str
+    message: str
+    messages:Annotated[list, operator.add]
+    changeme: str
+
+
+async def call_model(state: State, config: RunnableConfig) -> Dict[str, Any]:
+    """Process input and returns output.
+
+    Can use runtime configuration to alter behavior.
+    """
+    configuration = config["configurable"]
+    result = await llm.ainvoke(state["messages"])
+    return {
+        "changeme": "output from call_model. " + f'Configured with {configuration.get("my_configurable_param")}',
+        "messages": [AIMessage(content=result.content)]
+    }
+
+
+# Define the graph
+graph = (
+    StateGraph(State, config_schema=Configuration)
+    .add_node(call_model)
+    .add_edge("__start__", "call_model")
+    .compile(name="New Graph")
+)
